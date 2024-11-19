60a903f1ece52ac1599ce5e0ec70a653a802a2aa
dante851 <vignesh.s98@cognizant.com>
2024-11-19 21:41:19 +0530

    Update SimpleConversationalBot.js

--- SimpleConversationalBot.js
+++ SimpleConversationalBot.js
@@ -59,8 +59,10 @@ module.exports = {
         console.log("on_event -->  Event : ", JSON.stringify(data));
         if(data.event.completedTaskName === "welcome task"){
              console.log("completedTaskName --> ");
-data.context.session.BotUserSession.noOfconsecutiveFailedAttempts = 0;
-data.context.session.BotUserSession.errorValidation = false;
+            BotUserSession.put("noOfconsecutiveFailedAttempts", 0);
+            BotUserSession.put("errorValidation", false);
+// data.context.session.BotUserSession.noOfconsecutiveFailedAttempts = 0;
+// data.context.session.BotUserSession.errorValidation = false;
         }else{
         console.log("else -->")
         }
