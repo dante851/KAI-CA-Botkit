c863296f3f7719cd0d681ddfbae48a3b1571f41b
dante851 <vignesh.s98@cognizant.com>
2024-11-21 15:28:49 +0530

    Update utility.js

--- utility.js
+++ utility.js
@@ -73,33 +73,33 @@ module.exports = {
     }
   },
 };
- msgTemplate: function (templateData) {
-    const templateType = templateData[0].MEDIA_TYPE;
-    const tableTemplate = templateData[0].DATA
-      ? [
-          {
-            type: "text",
-            component: {
-              type: "template",
-              payload: {
-                template_type: "table",
-                ...JSON.parse(templateData[0].DATA),
-              },
-            },
-            cInfo: {
-              body: "Account details",
+function msgTemplate(templateData) {
+  const templateType = templateData[0].MEDIA_TYPE;
+  const tableTemplate = templateData[0].DATA
+    ? [
+        {
+          type: "text",
+          component: {
+            type: "template",
+            payload: {
+              template_type: "table",
+              ...JSON.parse(templateData[0].DATA),
             },
           },
-        ]
-      : null;
+          cInfo: {
+            body: "Account details",
+          },
+        },
+      ]
+    : null;
 
-    const dafaultTextTemplate = templateData[0].WEB_RESPONSE_MSG;
+  const dafaultTextTemplate = templateData[0].WEB_RESPONSE_MSG;
 
-    switch (templateType) {
-      case "TABLE":
-        return tableTemplate;
+  switch (templateType) {
+    case "TABLE":
+      return tableTemplate;
 
-      default:
-        return dafaultTextTemplate;
-    }
+    default:
+      return dafaultTextTemplate;
   }
+}
